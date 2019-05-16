c294d9fc6503d40195fcc1a171d17bd66541d4e4
lichuan01 <lichuan01@baidu.com>
2019-05-16 15:03:54 +0800

    显示图片和链接

--- go/web_cmd/make.sh
+++ go/web_cmd/make.sh
@@ -34,4 +34,13 @@ select_fields____0____1____3____5____7|match_str____广告费____移动|print_ht
 |
 http://localhost:8000//static/temp/select_fields____0____1____3____5____7__print_html_table_20190515191223_20109600.input.txt
 这一行无效
+
+
+d. 例子4
+print_html_image____key____image____text____text____url
+
+
+1   http://img10.360buyimg.com/n7/jfs/t1/7015/30/2122/109896/5bd1598dE5bbab285/9b2b9b89d823218e.jpg 海信（Hisense）HZ55E5A 55英寸 超高清    3368    https://item.jd.com/100000384561.html   
+2   http://img11.360buyimg.com/n7/jfs/t1/8073/22/3604/363230/5bd75206E77c5e1ff/3e98ee776e76f4e8.jpg 创维（SKYWORTH）65H5 65英寸4K超高   4001    https://item.jd.com/100000384561.html
+
 '''
--- go/web_cmd/process_line.py
+++ go/web_cmd/process_line.py
@@ -47,6 +47,60 @@ def print_html_table():
         i += 1
     print '</table>'
     
+def print_html_image(tags):
+    types = dict([(i, tags[i]) for i in range(0, len(tags))])
+    print '''
+    <script type="text/javascript">
+        function change(){
+            var show_analysis_data_label = document.getElementById("show_analysis_data_label");
+            show_analysis_data_label.innerHTML = '';
+            var c_u_ids = document.getElementsByName("show_analysis_data_id");
+            var str = '选中prodId: ';
+            hit = 0
+            for (var i = 0; i < c_u_ids.length; i++) {
+                if (c_u_ids[i].checked) {
+                    str += c_u_ids[i].value;
+                    str += "    ";
+                    hit += 1;
+                }
+            }
+            p = hit / c_u_ids.length
+            p = '' + p
+            hit = '' + hit
+            n = '' + c_u_ids.length
+            str = '总数: ' + n + '<br>选中: ' + hit + '<br>命中率: ' + p + '<br>' + str
+            show_analysis_data_label.innerHTML = str;
+        }
+    </script>
+    <div>
+    '''
+    i = 0
+    for line in sys.stdin:
+        line = line[:-1]
+        line = line.decode('utf8', 'ignore')
+        fields = line.split('\t')
+        keys = [fields[i] for i, t in types.items() if t == 'key']
+        imgs = [fields[i] for i, t in types.items() if t == 'image']
+        urls = [fields[i] for i, t in types.items() if t == 'url']
+        texts = [fields[i] for i, t in types.items() if t == 'text']
+        key = keys[0] if len(keys) > 0 else i
+        i += 1
+        key_html = '<label>%s<input type="checkbox" name="show_analysis_data_id" value="%s" onclick="change()"/></label>' % (key, key)
+
+        url_html = '|'.join(['<a href="%s">%s</a>' % (url, url[:15]) for url in urls])
+
+        text_html = '|'.join(['<span width=100>%s</span>' % text for text in texts])
+
+        img_html = []
+        for v in imgs: 
+            red = ' style="border:3px solid #ff0000" ' if v.find("select_color=1") != -1 else ' style="border:3px solid #000000" '
+            img_html.append('<img src="%s" width=150  %s />' % (v, red))
+        img_html = '\t'.join(img_html)
+        output = [key_html, img_html, url_html, text_html, '<hr>'] 
+        print '\t'.join(output).encode('utf8', 'ignore')
+    print '<hr><label id = "show_analysis_data_label" style="width: 1000px; height: 100px;"></label>'
+    print '</div>'
+
 
 if __name__ == "__main__":
     func_arg = sys.argv[1]
